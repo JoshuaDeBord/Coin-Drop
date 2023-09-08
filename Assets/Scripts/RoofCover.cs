9eeba8169faa839f40c83d731f999271e2bd0be1
JoshuaDeBord <joshua.debord@gctcok.edu>
2023-09-08 15:08:58 -0500

    New stuff
    Added coin
    Added Collider roof trigger
    Added Walls
    Added Background

---
+++ Assets/Scripts/RoofCover.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofCover : MonoBehaviour
+{
+
+    public Collider ColliderFalse;
+    public MeshRenderer RendererEnabler;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ColliderFalse.isTrigger = false;
+        RendererEnabler.enabled = true;
+    }
+}

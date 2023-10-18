999d8c3ebaae07607d5ba1c09bcf7d814a2b94d6
JoshuaDeBord <joshua.debord@gctcok.edu>
2023-10-18 15:29:41 -0500

    added

--- Assets/Scripts/OnOffColorChangeHighGravity.cs
+++ Assets/Scripts/OnOffColorChangeHighGravity.cs
@@ -1,42 +1,40 @@
-using System.Linq.Expressions;
 using System.Threading;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OnOffColorChange : MonoBehaviour
+public class OnOffColorChangeHighGravity : MonoBehaviour
 {
     public Image ButtonSettings;
     public TextMeshProUGUI OnOffLabel;
     public Image ButtonSettingsMainMenu;
     public TextMeshProUGUI OnOffLabelMainMenu;
     public bool isOn = true;
-    
+    public DropTheCoin DropTheCoin;
     public void ColorOffOn()
     {
         isOn = !isOn;
         Thread.Sleep(50);
 
-        if (isOn == true && ButtonSettings.color != new Color32(15, 255, 0, 255))
+        if (isOn == true && ButtonSettings.color != new Color32(15, 255, 0, 255) && DropTheCoin.dropButtonPressed == false)
         {
             ButtonSettings.color = new Color32(15, 255, 0, 255);
             OnOffLabel.SetText("ON");
             OnOffLabel.fontSize = 191.76f;
 
+            ButtonSettingsMainMenu.color = new Color32(15, 255, 0, 255);
             OnOffLabelMainMenu.SetText("ON");
             OnOffLabelMainMenu.fontSize = 191.76f;
-
         }
-        else if (isOn == false && ButtonSettings.color != new Color32(255, 0, 2, 255))
+        else if (isOn == false && ButtonSettings.color != new Color32(255, 0, 2, 255) && DropTheCoin.dropButtonPressed == false)
         {
             ButtonSettings.color = new Color32(255, 0, 2, 255);
             OnOffLabel.SetText("OFF");
             OnOffLabel.fontSize = 150;
 
+            ButtonSettingsMainMenu.color = new Color32(255, 0, 2, 255);
             OnOffLabelMainMenu.SetText("OFF");
             OnOffLabelMainMenu.fontSize = 150;
-
         }
     }
 }
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,30 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-
 using UnityEngine;
 
 public class Settings : MonoBehaviour
-{
+{ 
+
+    public DropTheCoin DropTheCoin;
     public Rigidbody CoinMain;
     public bool GravityHigh = true;
     public PhysicMaterial BouncyCoin;
-    public DropTheCoin DropTheCoin;
 
 
     public void HighGravitySetting()
     {
         if (GravityHigh == true && DropTheCoin.dropButtonPressed == false)
         {
-            
-            
-            
+
+            CoinMain.mass = 1000;
+            CoinMain.drag = 2.10f;
+            BouncyCoin.bounciness = 1f;
+
             GravityHigh = false;
         }
         else if (GravityHigh == false && DropTheCoin.dropButtonPressed == false)
         {
-            
+            CoinMain.mass = 3000;
+            CoinMain.drag = 0;
+            BouncyCoin.bounciness = 0.55f;
+
             GravityHigh = true;
         }
-
     }
+
+
+
+
 }
